2c46be0d5c5251acb3a29776ec45ac6138fbb063
MarioGabIFP <IFPmarionunezinfor@gmail.com>
2022-03-14 09:29:10 +0100

    Añado contenido de PLSQL

---
+++ PLSQL/PLSQL.sql
@@ -0,0 +1,16 @@
+drop database if exists red;
+create database red;
+use red;
+
+create table if not exists usuarios(
+	cod int primary key auto_increment,
+	nombre char(50) not null,
+	ape1 char(50) not null,
+	ape2 char(50) not null,
+	fnaci date,
+	edad double,
+	usuario char(15),
+	cp char(5),
+	falta timestamp default current_timestamp,
+	perfil char(10)
+);
